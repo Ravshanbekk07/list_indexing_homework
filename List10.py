26cf276aefd6664d472a80b27e823be7cb392882
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-07 17:21:00 +0500

    better

--- List10.py
+++ List10.py
@@ -6,4 +6,14 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+    max  = 0
+    k = 0
+    while k<len(list_num):
+        if max<list_num[k]:
+            max = list_num[k]
+        k+=1
+    return max
+    
+
+v = main([12, 2, 5, 2, 7, 9, 1])
+print(v)
